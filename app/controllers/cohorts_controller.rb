6d47aa0d0a4e12d1aea3584cb53c236cba4decc3
Tom Barnes <barnes.thomas.c@gmail.com>
2018-08-21 22:33:59 -0400

    set up controllers

---
+++ app/controllers/cohorts_controller.rb
@@ -0,0 +1,25 @@
+class CohortsController < ApplicationController
+    def index
+        # method: get
+        # action: index
+        # template: users/index.html.erb
+    end
+
+    def show
+    # method: get
+    # action: index
+    # template: users/show.html.erb
+    end
+
+    def new
+    # method: get
+    # action: index
+    # template: users/new.html.erb
+    end
+
+    def edit
+    # method: get
+    # action: index
+    # template: users/edit.html.erb
+    end
+end
